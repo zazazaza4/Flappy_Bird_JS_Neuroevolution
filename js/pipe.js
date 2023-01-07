0b2592a16bfa67233f7c09a74c6d569804abc16c
Yaroslav <6715414@gmail.com>
2023-01-07 20:25:51 +0200

    added a basic pair of pipes

--- js/pipe.js
+++ js/pipe.js
@@ -1,21 +1,31 @@
 class Pipe {
-  constructor(isTop, heigth) {
+  constructor(isTop, height) {
     this.width = 100;
-    this.heigth = heigth;
+    this.height = height;
     this.isTop = isTop;
     this.posX = canvas.width;
 
     if (isTop) {
       this.topY = 0;
-      this.bottomY = this.heigth;
+      this.bottomY = this.height;
     } else {
-      this.topY = canvas.heigth - this.heigth;
-      this.bottomY = canvas.heigth;
+      this.topY = canvas.height - this.height;
+      this.bottomY = canvas.height;
     }
   }
 
-  update(speed = 8) {
-    this.posX -= speed * 5;
+  draw() {
+    fill(0, 204, 0);
+    if (this.isTop) {
+      rect(this.posX, this.topY + this.height - 800, this.width, this.height);
+    } else {
+      rect(this.posX, this.topY, this.width, this.height);
+    }
+  }
+
+  update(speed = 4) {
+    this.posX -= speed;
+    this.draw();
   }
 
   isCollisionPlayer(player) {
--- js/pipePair.js
+++ js/pipePair.js
@@ -1,14 +1,36 @@
 class PipePair {
-  constructor() {
+  constructor(firstPipe) {
     this.gap = 160;
+    this.isPassed = false;
 
-    this.bottomPipe = new Pipe(false);
-    this.topPipe = new Pipe(true);
+    this.topHeight = (canvas.height - 30) / 1.5;
+    this.bottomHeight = canvas.height - this.topHeight / 2 - this.gap;
+    this.bottomPipe = new Pipe(false, this.bottomHeight);
+    this.topPipe = new Pipe(true, this.topHeight);
   }
 
   update() {
     this.bottomPipe.update();
     this.topPipe.update();
+  }
+
+  playerPassed(playerX) {
+    if (
+      !this.isPassed &&
+      playerX > this.bottomPipe.posX + this.bottomPipe.width
+    ) {
+      this.isPassed = true;
+      return true;
+    }
+
+    return false;
+  }
+
+  offScreen() {
+    if (this.bottomPipe.posX + this.bottomPipe.width < 0) {
+      return true;
+    }
+    return false;
   }
 
   setNewPositionX(x) {
