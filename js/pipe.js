0e663ba8f33009c0974521f8494c6869df6a5e67
Yaroslav <6715414@gmail.com>
2023-01-08 11:15:31 +0200

    added image and changed isCollisionPlayer

--- js/pipe.js
+++ js/pipe.js
@@ -1,9 +1,9 @@
 class Pipe {
-  constructor(isTop, height) {
+  constructor(isTop, height, addPosX) {
     this.width = 100;
     this.height = height;
     this.isTop = isTop;
-    this.posX = canvas.width;
+    this.posX = canvas.width + addPosX;
 
     if (isTop) {
       this.topY = 0;
@@ -17,9 +17,9 @@ class Pipe {
   draw() {
     fill(0, 204, 0);
     if (this.isTop) {
-      rect(this.posX, this.topY + this.height - 800, this.width, this.height);
+      image(pipeTopSprite, this.posX, this.topY + this.height - 800);
     } else {
-      rect(this.posX, this.topY, this.width, this.height);
+      image(pipeBottomSprite, this.posX, this.topY);
     }
   }
 
@@ -29,10 +29,17 @@ class Pipe {
   }
 
   isCollisionPlayer(player) {
-    if (player.x + player.size / 2 >= this.posX) {
-      return true;
+    if (
+      player.x + player.size / 2 >= this.posX &&
+      player.x - player.size / 2 <= this.posX + this.width
+    ) {
+      if (!this.isTop && player.y + player.size / 2 >= this.topY) {
+        return true;
+      }
+      if (this.isTop && player.y - player.size / 2 <= this.bottomY) {
+        return true;
+      }
     }
-
     return false;
   }
 }
