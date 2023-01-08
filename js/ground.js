dde0eada0a582d6e2290937a12c27e9e0df110be
Yaroslav <6715414@gmail.com>
2023-01-08 11:12:08 +0200

    added ground

--- js/ground.js
+++ js/ground.js
@@ -1,5 +1,28 @@
 class Ground {
-  constructor() {}
+  constructor() {
+    this.height = 30;
+    this.topPixelCoord = canvas.height - this.height;
+    this.pixelOffset = 0;
+  }
 
-  update() {}
+  draw() {
+    fill(0);
+    rect(0, this.topPixelCoord, canvas.width, this.height);
+    for (var i = this.pixelOffset; i < canvas.width; i += groundSprite.width) {
+      image(groundSprite, i, this.topPixelCoord);
+    }
+  }
+
+  update(isMove, speedGround = 3) {
+    let speed = isMove ? speedGround : 0;
+    this.pixelOffset -= speed;
+    if (this.pixelOffset <= -groundSprite.width) {
+      this.pixelOffset += groundSprite.width;
+    }
+    this.draw(isMove);
+  }
+
+  isCollisionPlayer(player) {
+    return player.y + player.size / 2 >= this.topPixelCoord;
+  }
 }
