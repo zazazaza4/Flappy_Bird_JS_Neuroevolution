86b636136b4b3a729441bd6438c8916a81c01064
Yaroslav <6715414@gmail.com>
2023-01-10 20:45:18 +0200

    change  func keyPressed

--- js/sketch.js
+++ js/sketch.js
@@ -152,10 +152,9 @@ function keyPressed() {
       if (bird) {
         saveJSON(bird.brain, "brain.json");
       }
+      break;
     case "p":
       init(true);
-
-    default:
       break;
   }
 }
